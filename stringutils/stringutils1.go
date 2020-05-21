4851587e9b797f8bdf656d3d9976e205d3bd592f
Vlad Bezden <vlad.bezden@gmail.com>
2020-05-21 13:18:50 -0400

    :ok_hand: DOC: Add documentation

--- stringutils/stringutils1.go
+++ stringutils/stringutils1.go
@@ -2,10 +2,12 @@ package stringutils
 
 import "strings"
 
+//Upper converts string to uppercase
 func Upper(s string) string {
 	return strings.ToUpper(s)
 }
 
+//Lower converts string to lowercase
 func Lower(s string) string {
 	return strings.ToLower(s)
 }
--- stringutils/stringutils2.go
+++ stringutils/stringutils2.go
@@ -2,6 +2,7 @@ package stringutils
 
 import "strings"
 
+//Twice repeats string twice
 func Twice(s string) string {
 	return strings.Repeat(s, 2)
 }
