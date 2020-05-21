82e8d61757030ff5cf10e237479bdc0189245c20
Vlad Bezden <vlad.bezden@gmail.com>
2020-05-21 13:18:37 -0400

    :ok_hand: DOC: Add documentation

--- main.go
+++ main.go
@@ -5,22 +5,6 @@ import (
 	su "modules/stringutils"
 )
 
-// const state = "New Jersey"
-
-// var name string
-
-// func main() {
-// 	name = "Vlad"
-// 	from := "USA"
-// 	var n int
-
-// 	fmt.Printf("Hello, fellow %s Gophers!\n", state)
-// 	fmt.Printf("My name is %s and I'm from %s.\n", name, from)
-// 	fmt.Printf("My name is %s and I'm from %s.\n", name, from)
-// 	fmt.Printf("By the time %d o'clock comes around, we'll know how to code in Go\n\n", n)
-// 	fmt.Println("Let's get started")
-// }
-
 func main() {
 	fmt.Println("Upper:", su.Upper("upper test"))
 	fmt.Println("Lower:", su.Lower("LOWER TEST"))
